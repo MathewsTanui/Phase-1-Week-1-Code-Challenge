e8276914604b0899f94f26576d0ec6b67b9e605f
MathewsTanui <tanuimathews02@gmail.com>
2024-03-23 10:28:20 +0300

    third grade

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ function generateGrade(marks) {
         return "A";
     } else if (marks >= 60 && marks <= 79) {
         return "B";
+    } else if (marks >= 50 && marks <= 59) {
+        return "C";
     } 
 }
