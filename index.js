7448e637faa0716b4b8c8d67bc3c72fa376bcdef
MathewsTanui <tanuimathews02@gmail.com>
2024-03-23 10:33:26 +0300

    complete speed detector

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function generateGrade(marks) {
         return "E";
     }
 }
+
+function calculateDemeritPoints(speed) {
+    const speedLimit = 70;
+    const kmPerDemerit = 5;
+    const pointsPerDemerit = 1;
+
+    if (speed <= speedLimit) {
+        return "Ok";
+    } else {
+        const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemerit) * pointsPerDemerit;
+        if (demeritPoints > 12) {
+            return "License suspended";
+        } else {
+            return `Points: ${demeritPoints}`;
+        }
+    }
+}
