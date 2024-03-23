2ff52f2c9b5c59c7b156957adc706e7397c749f4
MathewsTanui <tanuimathews02@gmail.com>
2024-03-23 10:36:44 +0300

    complete net salary calculator

--- index.js
+++ index.js
@@ -28,3 +28,25 @@ function calculateDemeritPoints(speed) {
         }
     }
 }
+
+function calculateNetSalary(basicSalary, benefits) {
+    const taxRate = 0.3;
+    const nhifRate = 0.05;
+    const nssfRate = 0.1;
+
+    const grossSalary = basicSalary + benefits;
+    const tax = grossSalary * taxRate;
+    const nhifDeduction = grossSalary * nhifRate;
+    const nssfDeduction = grossSalary * nssfRate;
+    const netSalary = grossSalary - tax - nhifDeduction - nssfDeduction;
+
+    return {
+        grossSalary,
+        tax,
+        nhifDeduction,
+        nssfDeduction,
+        netSalary
+    };
+}
+
+
