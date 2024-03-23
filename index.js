f91a5e986bd2d5bd2cc1782beac3dbf843bad8ac
MathewsTanui <tanuimathews02@gmail.com>
2024-03-23 10:30:28 +0300

    fourth and fifth grade

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ function generateGrade(marks) {
         return "B";
     } else if (marks >= 50 && marks <= 59) {
         return "C";
-    } 
+    } else if (marks >= 40 && marks <= 49) {
+        return "D";
+    } else {
+        return "E";
+    }
 }
